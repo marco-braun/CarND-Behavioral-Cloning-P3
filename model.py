429b180efe3b9726b118fdee4ac81148aa45c4a5
Marco Braun <braunmarco93@gmail.com>
2021-01-03 20:34:45 +0100

    adjusted_training_skript

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
-import numpy as np
-import keras
-import csv
+import argparse
 import cv2
+import csv
+import keras
+from keras.callbacks import CSVLogger
+import numpy as np
 import os
 from tqdm import tqdm
 
@@ -10,23 +12,42 @@ class steering_model(object):
     def __init__(self):
         self.layers = [{'type': "dense", 'units': 1}]
 
-    def preprocessing(self):
+    def preprocessing_flatten(self):
         self.model.add(keras.layers.Flatten(input_shape=(160, 320, 3)))
 
-    def def_model(self):
+    def def_model_dense(self):
 
         self.model = keras.models.Sequential()
-        self.preprocessing()
+        self.preprocessing_flatten()
         for layer in self.layers:
             if layer['type'] == 'dense':
                 self.model.add(keras.layers.Dense(layer['units']))
 
         return self.model
 
+    def def_model_lenet(self):
 
-def main(log_results, model_name):
+        self.model = keras.Sequential()
 
-    data_path = '../../../../../Volumes/home/Projekte/Data/CarND-Behavioral-Cloning-P3/data_recording'
+        self.model.add(keras.layers.Conv2D(filters=6, kernel_size=(3, 3), activation='relu'))
+        self.model.add(keras.layers.AveragePooling2D())
+
+        self.model.add(keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu'))
+        self.model.add(keras.layers.AveragePooling2D())
+
+        self.model.add(keras.layers.Flatten())
+
+        self.model.add(keras.layers.Dense(units=120, activation='relu'))
+
+        self.model.add(keras.layers.Dense(units=84, activation='relu'))
+
+        self.model.add(keras.layers.Dense(units=1))
+
+        return self.model
+
+
+def main(log_results, model_name, data_path):
+    callbacks = []
     data = []
 
     with open(data_path + '/driving_log.csv') as csv_file:
@@ -37,7 +58,7 @@ def main(log_results, model_name):
 
     input_images_paths_old = data[:, 1]
 
-    Label_data_str = data[:, 6]
+    Label_data_str = data[:, 3] #steering
     Input_image_data = []
     Label_data = []
     print("Extracting images from repository ...")
@@ -47,21 +68,39 @@ def main(log_results, model_name):
         Input_image_data.append(cv2.imread(path_new))
 
         Label_data.append(float(Label_data_str[i]))
+        #break
 
-    Input_image_data = np.array(Input_image_data)
+    Input_image_data = np.array(Input_image_data, dtype=np.float32)
     Label_data = np.array(Label_data)
 
 
-    model = steering_model().def_model()
+    model = steering_model().def_model_dense()
 
     model.compile(loss='mse', optimizer='adam')
-    model.fit(Input_image_data, Label_data, validation_split=0.2, shuffle=True, epochs=20, batch_size=32)
+    if log_results==True:
+        dir_name = "Log_training_" + model_name
+        if not os.path.exists(dir_name):
+            os.mkdir(dir_name)
+        callbacks.append(CSVLogger(os.path.join(dir_name, 'training_log.csv'), append=True, separator=';'))
+    model.fit(Input_image_data, Label_data, validation_split=0.2, shuffle=True, epochs=20, batch_size=32, callbacks=callbacks)
 
     if log_results == True:
-        model.save(os.path.join('trained_models', 'model_{}.h5'.format(model_name)))
+        model.save(os.path.join(dir_name, 'model_{}.h5'.format(model_name)))
+
+
 
 
 if __name__ == "__main__":
 
-    model_name = "Dense"
-    main(log_results=True, model_name=model_name)
+    parser = argparse.ArgumentParser(description="Train Setup")
+    parser.add_argument(
+        'data_path',
+        type=str,
+        help='Path to the data',
+        nargs='?',
+        default='../../../../../Volumes/home/Projekte/Data/CarND-Behavioral-Cloning-P3/data_recording'
+    )
+    args = parser.parse_args()
+
+    model_name = "LeNet"
+    main(log_results=True, model_name=model_name, data_path=args.data_path)
