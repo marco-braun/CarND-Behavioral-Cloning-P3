c23d04d228d3fd751d94476810a8cc4b15103da0
Marco Braun <braunmarco93@gmail.com>
2021-01-09 23:53:08 +0100

    adjusted_training_skript

--- train_model.py
+++ train_model.py
@@ -6,6 +6,8 @@ from keras.callbacks import CSVLogger
 import numpy as np
 import os
 from tqdm import tqdm
+from keras.layers import Lambda, Cropping2D
+
 
 class steering_model(object):
 
@@ -15,9 +17,13 @@ class steering_model(object):
     def preprocessing_flatten(self):
         self.model.add(keras.layers.Flatten(input_shape=(160, 320, 3)))
 
-    def def_model_dense(self):
+    def def_model_arbitrary(self):
 
         self.model = keras.models.Sequential()
+
+        self.model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
+        self.model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
+
         self.preprocessing_flatten()
         for layer in self.layers:
             if layer['type'] == 'dense':
@@ -28,6 +34,9 @@ class steering_model(object):
     def def_model_lenet(self):
 
         self.model = keras.Sequential()
+
+        self.model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
+        self.model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160, 320, 3)))
 
         self.model.add(keras.layers.Conv2D(filters=6, kernel_size=(3, 3), activation='relu'))
         self.model.add(keras.layers.AveragePooling2D())
@@ -61,20 +70,25 @@ def main(log_results, model_name, data_path):
     Label_data_str = data[:, 3] #steering
     Input_image_data = []
     Label_data = []
+
+
     print("Extracting images from repository ...")
     for i in tqdm(range(data.shape[0])):
 
         path_new = os.path.join(data_path, 'IMG', input_images_paths_old[i].split('/')[-1])
-        Input_image_data.append(cv2.imread(path_new))
 
+        Input_image_data.append(cv2.imread(path_new))
         Label_data.append(float(Label_data_str[i]))
-        #break
+
+        # flipped recording
+        Input_image_data.append(np.fliplr(cv2.imread(path_new)))
+        Label_data.append(-float(Label_data_str[i]))
 
     Input_image_data = np.array(Input_image_data, dtype=np.float32)
     Label_data = np.array(Label_data)
 
 
-    model = steering_model().def_model_dense()
+    model = steering_model().def_model_lenet()
 
     model.compile(loss='mse', optimizer='adam')
     if log_results==True:
@@ -89,7 +103,6 @@ def main(log_results, model_name, data_path):
 
 
 
-
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Train Setup")
@@ -98,9 +111,9 @@ if __name__ == "__main__":
         type=str,
         help='Path to the data',
         nargs='?',
-        default='../../../../../Volumes/home/Projekte/Data/CarND-Behavioral-Cloning-P3/data_recording'
+        default='../../../../../Volumes/home-1/Projekte/Data/CarND-Behavioral-Cloning-P3/data_recording'
     )
     args = parser.parse_args()
 
-    model_name = "LeNet"
+    model_name = "LeNet_data_augment"
     main(log_results=True, model_name=model_name, data_path=args.data_path)
